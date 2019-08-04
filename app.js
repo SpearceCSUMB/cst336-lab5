6df4eae95dda685cb9c1c2fee84ea10c9010d67b
spearce <spearce@csumb.edu>
2019-08-03 20:53:24 -0400

    added heart Icon

--- app.js
+++ app.js
@@ -86,6 +86,7 @@ app.get("/api/displayFavorites", function(req, res) {
     conn.query(sql, sqlParams, function(err, results) {
       if (err) throw err;
       res.send(results);
+      console.log("FUCK FUCK FUCK FUCK " + results);
     });//query
   });//connect
   
